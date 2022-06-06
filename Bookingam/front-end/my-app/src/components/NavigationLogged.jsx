c4d4ef6a3aa8aaee6c9c3cdb703ca59fa5415e82
RobertCroitor <79307887+RobertCroitor@users.noreply.github.com>
2022-06-06 22:21:13 +0300

    Donation

--- Bookingam/front-end/my-app/src/components/NavigationLogged.jsx
+++ Bookingam/front-end/my-app/src/components/NavigationLogged.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./componentsCSS/navigation.css";
 import { getAuth, signOut } from "firebase/auth";
-
+import { Button } from "react-bootstrap";
 const NavigationLogged = () => {
   const signout = async (e) => {
     e.preventDefault();
@@ -28,17 +28,17 @@ const NavigationLogged = () => {
           </NavLink>
           <div className="navClass">
             <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+            <Button onClick={() => window.open( "https://www.paypal.com/donate/?hosted_button_id=D8VF5YS5WCAVE")}
+            style={{color:"silver", background: "rgb(29, 30, 31)", boxShadow: "none", border: "0",marginTop:"2px"}}>Donate</Button>
+              </li>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/uploadBook">
                   <div className="navtext">Upload Book</div>
                 </NavLink>
               </li>
 
-              {/* <li className="nav-item">
-                <NavLink className="nav-link" to="/Account">
-                  Account
-                </NavLink>
-              </li> */}
+              
               <li className="nav-item">
                 <NavLink className="nav-link" to="/myShelf">
                   <div className="navtext">Favorite Books</div>
