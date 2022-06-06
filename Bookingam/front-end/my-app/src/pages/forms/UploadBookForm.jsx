5799e7d38dae5d48cdbc848c2e7c7c54cd4ff3e2
RobertCroitor <79307887+RobertCroitor@users.noreply.github.com>
2022-06-06 12:10:46 +0300

    Upload Form

--- Bookingam/front-end/my-app/src/pages/forms/UploadBookForm.jsx
+++ Bookingam/front-end/my-app/src/pages/forms/UploadBookForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../pagesCSS/form.css";
 import firebase from "../creditentials/initFB";
-
+import "../pagesCSS/login_register.css"
 function UploadBookForm() {
   const [title, setTitle] = useState("");
 
@@ -18,10 +18,35 @@ function UploadBookForm() {
     todoRef.push(todo);
   };
   return (
-    <div className="form-group">
-      <input type="text" onChange={handleOnChange} value={title}></input>
-      <button onClick={createTodo}>Upload </button>
+    <body>
+    <div className="form">
+      <div id="uploadForm" class="center">
+        <h1 >Upload Book</h1>
+        <form >
+          <div className="input-container">
+            <label>Title </label>
+            <input id="formID" type="text" name="title" placeholder="Title" />
+          </div>
+          <div className="input-container">
+          <label>Author </label>
+            <input id="formID" type="text" name="author" placeholder="Author" />
+          </div>
+          <div className="input-container">
+          <label>Genre </label>
+            <input id="formID" type="text" name="genre" placeholder="Genre" />
+          </div>
+          <div className="input-container">
+          <label>URL </label>
+            <input id="formID" type="url" name="url" placeholder="URL" />
+          </div>
+          <div className="button-container">
+            <button id="upload" class="btn btn-primary" type="submit">Upload</button>
+          </div>
+        </form>
+      </div>
+
     </div>
+  </body>
   );
 }
 
